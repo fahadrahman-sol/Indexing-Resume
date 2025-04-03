addc21345c53636da0c04eaf5b5d7d3d0afe0df8
fahadrahman-sol <frahman@siliconorchard.com>
2025-04-03 11:20:53 +0600

    updated config

--- app/core/config.py
+++ app/core/config.py
@@ -3,13 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from a .env file
 
-DB_NAME = os.getenv("DB_NAME", "resume_sol")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "1234")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 
-ES_HOST = os.getenv("ES_HOST", "http://localhost:9200")
-ES_INDEX = os.getenv("ES_INDEX", "resumes")
-ES_USERNAME = os.getenv("ES_USERNAME", "elastic")
-ES_PASSWORD = os.getenv("ES_PASSWORD", "changeme")
+ES_HOST = os.getenv("ES_HOST")
+ES_INDEX = os.getenv("ES_INDEX")
+ES_USERNAME = os.getenv("ES_USERNAME")
+ES_PASSWORD = os.getenv("ES_PASSWORD")
